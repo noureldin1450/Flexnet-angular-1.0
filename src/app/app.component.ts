1339055684ca8c46da7ff527735086e02e8f64ca
NourELdin <lolhack2017@gmail.com>
2020-07-24 13:15:06 -0700

    adding google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/Operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'flexnet';  
+
+  constructor(router:Router){
+
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+
+    // for every changing in the navigation
+    // navEndEvents.subscribe((event: NavigationEnd) =>{
+    //   gtag('config', 'UA-146859399-3',{
+    //     'page_path': event.urlAfterRedirects
+    //   });
+    // })
+
+  }
+
+
 }
 
