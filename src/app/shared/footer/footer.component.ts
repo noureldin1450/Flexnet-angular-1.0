addff987390bb36ced5c79af73c45c936d10aaaa
NourEldin <lolhack2017@gmail.com>
2020-08-05 02:58:40 -0700

    feat: new way to submit the resp

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -11,23 +11,38 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class FooterComponent implements OnInit {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   newsletterForm = new FormGroup({
     email: new FormControl('')
   })
 
-  onSubmit(){
+  onSubmit() {
     const body = new HttpParams()
       .set('form-name', 'newsletter')
       .append('email', this.newsletterForm.value.email)
-      this.http.post('/', body.toString(), {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      }).subscribe(res =>{
-        console.log('send succsesfuly')
-      }, err =>{
-        console.log('error'+ err)
-      })
+    this.http.post('/', body.toString(), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    }).subscribe(res => {
+      console.log('send succsesfuly')
+    }, err => {
+      if (err instanceof ErrorEvent) {
+        //client side error
+        alert("Something went wrong when sending your message.");
+        console.log(err.error.message);
+      } else {
+        //backend error. If status is 200, then the message successfully sent
+        if (err.status === 200) {
+          alert("Your message has been sent!");
+        } else {
+          alert("Something went wrong when sending your message.");
+          console.log('Error status:');
+          console.log(err.status);
+          console.log('Error body:');
+          console.log(err.error);
+        };
+      };
+    })
   }
 
 
